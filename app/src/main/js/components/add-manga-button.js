ef095ccfbcc5b6f0aafdffd611e41ab009d9f51a
Adrian Dela Piedra <adrian.onrails@gmail.com>
2018-05-26 20:32:40 +0800

    Add link to PLUS buttons

--- app/src/main/js/components/add-manga-button.js
+++ app/src/main/js/components/add-manga-button.js
@@ -1,12 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "../../res/scss/add-manga-button.scss";
 
 const AddMangaButton = () => (
   <div className="add-manga-button">
-    <button type="button" className="btn btn-primary rounded-circle add">
-      <span className="fa fa-plus" />
-    </button>
+    <Link to="/search">
+      <button type="button" className="btn btn-primary rounded-circle add">
+        <span className="fa fa-plus" />
+      </button>
+    </Link>
   </div>
 );
 
--- app/src/main/js/components/emptylist-placeholder.js
+++ app/src/main/js/components/emptylist-placeholder.js
@@ -1,13 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "../../res/scss/emptylist-placeholder.scss";
 
 const EmptyListPlaceholder = () => (
   <div className="wrapper transition-item page">
     <div className="empty-list-placeholder">
-      <button type="button" className="btn btn-primary rounded-circle add animated bounce infinite">
-        <span className="fa fa-plus" />
-      </button>
+      <Link to="/search">
+        <button
+          type="button"
+          className="btn btn-primary rounded-circle add animated bounce infinite">
+          <span className="fa fa-plus" />
+        </button>
+      </Link>
       <div className="label">Such empty, much wow!</div>
       <div className="recommendation">Click to download manga</div>
     </div>
