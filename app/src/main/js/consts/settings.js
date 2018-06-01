f8f3bd712f9de9ebe08a8c8819c0c93c0d71674e
Adrian Dela Piedra <adrian.onrails@gmail.com>
2018-06-01 22:20:53 +0800

    Hide the "db" files and changed default save location to the home dir

--- app/src/main/js/consts/settings.js
+++ app/src/main/js/consts/settings.js
@@ -1,11 +1,33 @@
-export const ALLMANGASDBNAME = "mangas.json";
-export const DOWNLOADEDMANGASDBNAME = "download_state.json";
-export const SETTINGSDBNAME = "settings.json";
+import os from "os";
+import path from "path";
+import mkdirp from "mkdirp";
+
+// base folder of all the settings
+const SETTINGSDIR = ".baruchmd";
+
+// create base folder
+mkdirp.sync(path.join(os.homedir(), SETTINGSDIR));
+
+export const ALLMANGASDBNAME = path.join(
+  os.homedir(),
+  SETTINGSDIR,
+  "mangas.json"
+);
+export const DOWNLOADEDMANGASDBNAME = path.join(
+  os.homedir(),
+  SETTINGSDIR,
+  "download_state.json"
+);
+export const SETTINGSDBNAME = path.join(
+  os.homedir(),
+  SETTINGSDIR,
+  "settings.json"
+);
 
 export default {
-    isUpdateOnStartEnabled: false,
-    saveLocation: "/mnt/data/stuff/manga",
-    saveFormat: "folder",
-    isNotifyOnFinishEnabled: true,
-    isDarkThemeEnabled: true,
+  isUpdateOnStartEnabled: false,
+  saveLocation: os.homedir(),
+  saveFormat: "folder",
+  isNotifyOnFinishEnabled: true,
+  isDarkThemeEnabled: true
 };
